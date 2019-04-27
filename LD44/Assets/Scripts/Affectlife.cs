e225157b538a5b5ba63b76d578e290395d6cab85
Enio Moura <mandosaldmer@gmail.com>
2019-04-27 12:55:00 -0300

    Commit inicial

---
+++ LD44/Assets/Scripts/Affectlife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Affectlife : MonoBehaviour
+{
+    public Slider barra;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void  click()
+    {
+        barra.value -= 5;
+    }
+}
---
+++ LD44/Assets/Scripts/DecisaoVida.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DecisaoVida : MonoBehaviour, IPointerClickHandler {
+    GameObject contadorVida;
+    Text textoVida;
+    int vida;
+
+    void Start () {
+        contadorVida = GameObject.Find ("contadorVida");
+    }
+
+    void Update () {
+        textoVida = contadorVida.GetComponent<Text> ();
+        vida = int.Parse (textoVida.text);
+    }
+
+    public void OnPointerClick (PointerEventData pointerEventData) {
+        switch (name) {
+            case "miojo":
+                textoVida.text = (vida - 50).ToString ();
+                break;
+            case "pizza":
+                textoVida.text = (vida - 50).ToString ();
+                break;
+            case "ifood":
+                textoVida.text = (vida - 50).ToString ();
+                break;
+            case "restaurante":
+                textoVida.text = (vida - 50).ToString ();
+                break;
+            default:
+                break;
+        }
+    }
+}
