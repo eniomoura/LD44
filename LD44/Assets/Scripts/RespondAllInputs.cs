7c95a1b092597417585897c99d1857269d964c67
Enio Moura <mandosaldmer@gmail.com>
2019-04-27 18:24:26 -0300

    Mentoria Programacao

---
+++ LD44/Assets/Scripts/RespondAllInputs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespondAllInputs : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButton(0)){
+            Utils.GetMasterUtils().ChangeScreen("WorkDay");
+        }
+    }
+}
--- LD44/Assets/Scripts/Utils.cs
+++ LD44/Assets/Scripts/Utils.cs
@@ -2,27 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Utils : MonoBehaviour
-{
+public class Utils : MonoBehaviour {
     public GameObject currentScreen;
     public GameObject[] screens;
 
-    public void ChangeScreen(string nextScreen){
-        foreach (GameObject screen in screens)
-        {
-            if(nextScreen.Equals(screen.name)){
-                currentScreen.SetActive(false);
-                screen.SetActive(true);
-                currentScreen=screen;
+    public void ChangeScreen (string nextScreen) {
+        foreach (GameObject screen in screens) {
+            if (nextScreen.Equals (screen.name)) {
+                currentScreen.SetActive (false);
+                currentScreen = screen;
+                screen.SetActive (true);
             }
         }
     }
 
-    public void EndConsequencia(){
-        
-    }
-
-    public static Utils GetMasterUtils(){
-        return GameObject.Find("Master").GetComponent<Utils>();
+    public static Utils GetMasterUtils () {
+        return GameObject.Find ("Master").GetComponent<Utils> ();
     }
 }
